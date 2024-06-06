49d11c54864eb56f28fb54c5bd01009b146b4e97
sebastian <sebastian.oyarzo.zapata@gmail.com>
2024-06-05 23:04:04 -0400

    finalizado3

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="navbar">
+    <nav className="navbar text-center">
       <Link to="/"> Home </Link> | <Link to="/favoritos"> Favoritos </Link>
     </nav>
   );
